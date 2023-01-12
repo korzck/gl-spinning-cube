6b4620b238e660f8cdc9cb5f039603c626e92823
korzck <konstantin.koretskiy@gmail.com>
2023-01-13 00:56:13 +0300

    simple window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
-#include <GLFW/glfw3.h>
+#include<GL/freeglut.h>
 
-int main()
+static void RenderSceneCB()
 {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+static void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(RenderSceneCB);
+}
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 01");
+
+    InitializeGlutCallbacks();
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    glutMainLoop();
+    
     return 0;
 }
